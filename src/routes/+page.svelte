d2b653a700c1218b439feb7930a590260952338f
Mike Hamrah <m@flowcode.com>
2022-09-04 10:55:52 -0400

    tailwind update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,6 @@
-<h1 class="text-3xl">
+
+<main class="grid place-items-center h-screen content-center">
+<h1 class="text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
     Michael Hamrah
-  </h1>
+</h1>
+</main>
