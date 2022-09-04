fd10c76569d56f2e957f3b7d6a930188df8de96d
Mike Hamrah <m@flowcode.com>
2022-09-04 15:28:34 -0400

    animation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,27 @@
+<main class="grid place-items-center h-screen content-center">
+	<h1
+		class="text-8xl background-animate font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600"
+	>
+		Michael Hamrah
+	</h1>
+</main>
 
 <style>
-    .background-animate {
-    background-size: 400%;
+	.background-animate {
+		background-size: 400%;
 
-    -webkit-animation: AnimationName 3s ease infinite;
-    -moz-animation: AnimationName 3s ease infinite;
-    animation: AnimationName 3s ease infinite;
-  }
+		-webkit-animation: AnimationName 3s ease infinite;
+		-moz-animation: AnimationName 3s ease infinite;
+		animation: AnimationName 3s ease infinite;
+	}
 
-  @keyframes AnimationName {
-    0%,
-    100% {
-      background-position: 0% 50%;
-    }
-    50% {
-      background-position: 100% 50%;
-    }
-  }
+	@keyframes AnimationName {
+		0%,
+		100% {
+			background-position: 0% 50%;
+		}
+		50% {
+			background-position: 100% 50%;
+		}
+	}
 </style>
-
-<main class="grid place-items-center h-screen content-center">
-<h1 class="text-8xl background-animate font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
-    Michael Hamrah
-</h1>
-</main>
